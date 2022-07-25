bbcf519baaae047fa2a4f134b7a02da613bbc503
likeorange <1664635775@qq.com>
2022-07-25 21:26:28 +0800

    修改配置文件

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
-
+const config = require('../config')
 const db = mysql.createPool({
-  host: '59.110.125.131',
-  port: 3306,
-  user:'db_OurBlog',
-  password:'BshPPkB7TCS6GG8Z',
-  database:'db_ourblog'
+  host: config.host,
+  port: config.port,
+  user: config.user,
+  password: config.password,
+  database: config.database
 })
 
 module.exports = db
