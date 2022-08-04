6c2bcc4dd3d51dde3a968146b4cb5d0c2fc5c1eb
likeorange <1664635775@qq.com>
2022-08-04 19:07:58 +0800

    修正关注功能

--- router_handler/follow.js
+++ router_handler/follow.js
@@ -4,23 +4,25 @@
  * @Author: likeorange
  * @Date: 2022-08-02 22:10:09
  * @LastEditors: likeorange
- * @LastEditTime: 2022-08-03 17:30:18
+ * @LastEditTime: 2022-08-04 19:01:02
  */
 const db = require('../db/index.js')
 const async = require('async')
 const SnowflakeID = require('../utils/SnowflakeID.js')
 exports.getFollowList = (req, res) => {
-  const sql = `select * from follow where follow.host_id = ${req.session.userInfo.id}`
+  const sql = `select * from follow where follow.guest_id = ${req.session.userInfo.id}`
   db.query(sql, (err, follows) => {
     if (err) return res.send(err)
     async.map(follows, (item, callback) => {
-      const sqluser = `select * from user where user.id = ${item.guest_id}`
+      const sqluser = `select * from user where user.id = ${item.host_id}`
       db.query(sqluser, (err, user) => {
         if (err) return res.send(err)
-        callback(null, user[0])
+        item = {...user[0]} 
+        callback(null, item)
       })
     }, function (err, results) {
       if (err) return res.send(err)
+      console.log(results);
       if (results.length != 0) {
         for (let data of results) {
           delete data.is_admin
@@ -30,7 +32,7 @@ exports.getFollowList = (req, res) => {
           delete data.create_time
         }
       }
-      return res.send({ code: 1, data: [...results] })
+      return res.send({ code: 1, data: results })
     })
 
   })
@@ -43,33 +45,33 @@ exports.addFollow = (req, res) => {
   });
   const id = snid.generate();
   const sqlInsert = 'insert into follow set ?'
-    db.query(sqlInsert, {id: id, host_id: req.params.hostId, guest_id: req.session.userInfo.id}, function (err, results) {
-      if (err) {
-        console.log(err);
-        return res.send({ code: 0, msg: '关注失败' })
-      }
-      if (results.affectedRows !== 1) {
-        console.log(results);
-        return res.send({ code: 0, msg: '关注用户失败，请稍后再试！' })
-      }
-      
-      res.send({code:1, msg:'关注成功'})
+  db.query(sqlInsert, { id: id, host_id: req.params.hostId, guest_id: req.session.userInfo.id }, function (err, results) {
+    if (err) {
+      console.log(err);
+      return res.send({ code: 0, msg: '关注失败' })
+    }
+    if (results.affectedRows !== 1) {
+      console.log(results);
+      return res.send({ code: 0, msg: '关注用户失败，请稍后再试！' })
+    }
+
+    res.send({ code: 1, msg: '关注成功' })
   })
 
 }
 
 exports.removeFollow = (req, res) => {
   const sqlInsert = 'delete from follow where follow.host_id=? and follow.guest_id =? '
-    db.query(sqlInsert, [req.params.hostId, req.session.userInfo.id], function (err, results) {
-      if (err) {
+  db.query(sqlInsert, [req.params.hostId, req.session.userInfo.id], function (err, results) {
+    if (err) {
 
-        return res.send({ code: 0, msg: '取关失败' })
-      }
-      if (results.affectedRows !== 1) {
-        console.log(results);
-        return res.send({ code: 0, msg: '取关失败，请稍后再试！' })
-      }
-      
-      res.send({code:1, msg:'取关成功'})
+      return res.send({ code: 0, msg: '取关失败' })
+    }
+    if (results.affectedRows !== 1) {
+      console.log(results);
+      return res.send({ code: 0, msg: '取关失败，请稍后再试！' })
+    }
+
+    res.send({ code: 1, msg: '取关成功' })
   })
 }
